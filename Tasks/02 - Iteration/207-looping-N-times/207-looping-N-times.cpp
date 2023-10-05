6d43efc02b38e7759adef83d0185817a93864b7d
Chris Moore <90791015+KurisuMoore@users.noreply.github.com>
2023-10-05 10:23:13 +0100

    iterationLabSession

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -2,27 +2,38 @@
 
 int main()
 {
-    //Counter variable
-    int i;
-
-
-    //For loop (pre-C99 standard)
-    for (i = 0; i < 5; i++)
-    {
-        //Perform code tasks
-        printf("i=%d\n", i);
-    }
 
     // TASK A - count from 10 down to 0 in steps of -1
 
+    for (int o = 10; o >= 0; o = o - 2)
+    {
+        printf("o = %d\n", o);
+    }
+
+
     // TASK B - count from 10 down to -10 in steps of -2
+
+    for (int p = 10; p >= -10; p = p - 2)
+    {
+        printf("p = %d\n", p);
+    }
 
     // TASK C - count from 10 to 200 in steps of 10
 
+    for (int k = 10; k <= 200; k = k + 10)
+    {
+        printf("k = %d\n", k);
+    }
+
     // TASK D - count from -10 down to 20 in steps of 5
 
+    for (int l = -10; l <= 20; l = l + 5)
+    {
+        printf("l = %d\n", l);
+    }
+
     // TASK E - can you predict the largest value of i that is displayed?
-    for (i = 0; i < 12; i += 3)
+    for (int i = 0; i < 12; i += 3)
     {
         //Perform code tasks
         printf("i=%d\n", i);
